665f632d3a80aaef4991b53c1a158fb804218a3f
thiagoTL <thiago.lourenco@deway.com.br>
2020-07-24 14:12:12 -0300

    Adicionando navigation bottom tabs

--- src/routes/BottomNavigator.js
+++ src/routes/BottomNavigator.js
@@ -2,6 +2,9 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import HomePage from "../pages/HomePage";
+import CardPage from '../pages/Card/CardPage'
+import ProfileSettings from '../pages/Profile/ProfileSettings'
+import MapPage from '../pages/Map/MapPage'
 
 const Bottom = createBottomTabNavigator();
 
@@ -9,6 +12,10 @@ export default function BottomNavigator() {
   return (
     <Bottom.Navigator>
       <Bottom.Screen name="HomePage" component={HomePage} />
+      <Bottom.Screen name="CardPage" component={CardPage} />
+      <Bottom.Screen name="MapPage" component={MapPage} />
+      <Bottom.Screen name="ProfileSettings" component={ProfileSettings} />
+
     </Bottom.Navigator>
   );
 }
