76e4a2f28afe246905f721c2e00c483a0d1b3c3d
thiagoTL <thiago.lourenco@deway.com.br>
2020-07-25 22:35:46 -0300

    Tela de Cards Settings feita

--- src/pages/Card/CardSettings/index.js
+++ src/pages/Card/CardSettings/index.js
@@ -1,18 +1,38 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useRef } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import Icon from "@expo/vector-icons/MaterialIcons";
-import Icons from "@expo/vector-icons/MaterialCommunityIcons";
 import IconF from "@expo/vector-icons/FontAwesome";
 import { Octicons } from "@expo/vector-icons";
 import { ScrollView } from "react-native-gesture-handler";
+import Animated, { multiply, divide } from "react-native-reanimated";
+import { useScrollHandler } from "react-native-redash";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+// ICONS
+import Icons from "@expo/vector-icons/MaterialCommunityIcons";
 
 import styles from "./styles";
 import { colors } from "../../../constants/colors";
+import visaLogo from "../../../assets/simbolos.png";
+import BollonScroll from "../../../components/BollonScroll";
 
 export default function CardSettigns() {
+  const navigation = useNavigation();
+  const data = [0, 1, 2];
+  const scroll = useRef(null);
+  const { scrollHandler, x } = useScrollHandler();
+
   return (
     <View style={styles.styles}>
       <View style={styles.header}>
+        <TouchableOpacity
+          style={styles.btnGoBack}
+          activeOpacity={0.7}
+          onPress={() => navigation.goBack()}
+        >
+          <Ionicons name="ios-arrow-back" size={24} color={colors.white} />
+        </TouchableOpacity>
         <View style={styles.viewHeaderTitle}>
           <Text style={styles.headerTitle}>Card Settings</Text>
         </View>
@@ -50,7 +70,104 @@ export default function CardSettigns() {
             <Text style={styles.viewCardIconText}>Change</Text>
           </View>
         </View>
-        <View style={styles.viewScroll} />
+
+        <View style={styles.viewScroll}>
+          <View style={styles.viewCardHeader}>
+            <Text style={styles.viewCardHeaderTitle}>All Cards</Text>
+            <Text style={styles.viewCardHeaderButton}>Add</Text>
+          </View>
+
+          <Animated.ScrollView
+            ref={scroll}
+            style={styles.viewScrollView}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            {...scrollHandler}
+          >
+            {data.map((item) => (
+              <View style={styles.viewCard} key={item}>
+                <View style={styles.viewCardHeaderT}>
+                  <Text style={styles.viewCardValue}>$2000.00</Text>
+                  <Image source={visaLogo} style={{ height: 40, width: 40 }} />
+                </View>
+                <View style={styles.viewCardNumber}>
+                  <Text style={styles.viewCardNumberText}>****</Text>
+                  <Text style={styles.viewCardNumberText}>****</Text>
+                  <Text style={styles.viewCardNumberText}>****</Text>
+
+                  <Text style={styles.viewCardNumberText}>1222</Text>
+                </View>
+                <View style={styles.viewCardFooter}>
+                  <View style={styles.viewCardFooterNameUser}>
+                    <Text style={styles.viewCardFooterNameText}>
+                      CARD HOLDER
+                    </Text>
+                    <Text style={styles.viewCardFooterNameTextValue}>
+                      THIAGO L. S. SILVA
+                    </Text>
+                  </View>
+                  <View style={styles.viewCardFooterName}>
+                    <Text style={styles.viewCardFooterNameText}>EXPIRES</Text>
+                    <Text style={styles.viewCardFooterNameTextValue}>
+                      02/22
+                    </Text>
+                  </View>
+                  <View style={styles.viewCardFooterName}>
+                    <Text style={styles.viewCardFooterNameText}>CVV</Text>
+                    <Text style={styles.viewCardFooterNameTextValue}>274</Text>
+                  </View>
+                </View>
+              </View>
+            ))}
+          </Animated.ScrollView>
+          <View style={styles.viewBollon}>
+            {data.map((item) => (
+              <BollonScroll />
+            ))}
+          </View>
+
+          <View style={styles.viewCardHeaderTrans}>
+            <Text style={styles.viewCardHeaderTitle}>Recent Transactions</Text>
+            <Text style={styles.viewCardHeaderButton}>Add</Text>
+          </View>
+          <View style={styles.viewCardTransactions}>
+            <View style={styles.viewCardTransactionsContent}>
+              <View style={styles.viewCardTransactionsContentBody}>
+                <View style={styles.viewTypeImageCard}>
+                  <Icons name="bank" size={26} color={colors.blue} />
+                </View>
+                <View style={styles.viewCardTransactionsDesc}>
+                  <Text style={styles.viewCardDataText}>Transport</Text>
+                  <Text style={styles.textNumberCardYear}>UBER Pool</Text>
+                </View>
+              </View>
+              <View style={styles.viewCardTransactionsDesc}>
+                <Text style={styles.viewCardDataText}>-$10.00</Text>
+                <Text style={styles.textNumberCardYearDate}>Aug 25</Text>
+              </View>
+            </View>
+            <View style={styles.viewLineTransactions} />
+            <View style={styles.viewCardTransactionsContent}>
+              <View style={styles.viewCardTransactionsContentBody}>
+                <View style={styles.viewTypeImageCard}>
+                  <Icons name="bank" size={26} color={colors.blue} />
+                </View>
+                <View style={styles.viewCardTransactionsDesc}>
+                  <Text style={styles.viewCardDataText}>Payment</Text>
+                  <Text style={styles.textNumberCardYear}>
+                    Payment from Ryan
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.viewCardTransactionsDesc}>
+                <Text style={[styles.viewCardDataText, { color: "#2ecc71" }]}>
+                  +$124.00
+                </Text>
+                <Text style={styles.textNumberCardYearDate}>Aug 25</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     </View>
   );
